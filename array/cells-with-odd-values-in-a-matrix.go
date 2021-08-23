5c757b15329118a1b59e3a122971cf872a683680
nurzhanuly <nurzhanuly@kolesa.kz>
2021-08-23 23:31:02 +0600

    not an optimized solution, using a 2D array

--- array/cells-with-odd-values-in-a-matrix.go
+++ array/cells-with-odd-values-in-a-matrix.go
@@ -4,9 +4,13 @@ func OddCells(m int, n int, indices [][]int) int {
 	matrix := make([][]int, m)
 	allocateMatrix(n, &matrix)
 
-	var rowIncrements = getRowIndices(indices)
-	var columnIncrements = getColumnIndices(indices)
+	var rowIndices = getIndices(indices, 0)
+	var columnIndices = getIndices(indices, 1)
 
+	incrementMatrixColumn(&matrix, columnIndices, m)
+	incrementMatrixRow(&matrix, rowIndices, n)
+
+	return countOddValues(&matrix, m, n)
 }
 
 func allocateMatrix(n int, matrix *[][]int) {
@@ -16,21 +20,52 @@ func allocateMatrix(n int, matrix *[][]int) {
 	}
 }
 
-func getRowIndices(indices [][]int) map[int]int {
-	rowIndices := make(map[int]int)
+func getIndices(indices [][]int, dimension int) map[int]int {
+	result := make(map[int]int)
 
 	for _, indicesSet := range indices {
-		for _, value := range indicesSet {
-			_, ok := rowIndices[value]
-			if ok {
-				rowIndices[value] += 1
-			} else {
-				rowIndices[value] = 1
-			}
+		value := indicesSet[dimension]
 
-			continue
+		_, ok := result[value]
+		if ok {
+			result[value] += 1
+		} else {
+			result[value] = 1
+		}
+
+		continue
+
+	}
+
+	return result
+}
+
+func incrementMatrixRow(matrix *[][]int, indices map[int]int, colLen int) {
+	for key := range indices {
+		for j := 0; j < colLen; j++ {
+			(*matrix)[key][j] += indices[key]
+		}
+	}
+}
+
+func incrementMatrixColumn(matrix *[][]int, indices map[int]int, rowLen int) {
+	for key := range indices {
+		for j := 0; j < rowLen; j++ {
+			(*matrix)[j][key] += indices[key]
+		}
+	}
+}
+
+func countOddValues(matrix *[][]int, rowLen, colLen int) int {
+	result := 0
+
+	for i := 0; i < rowLen; i++ {
+		for j := 0; j < colLen; j++ {
+			if (*matrix)[i][j]%2 == 1 {
+				result += 1
+			}
 		}
 	}
 
-	return rowIndices
+	return result
 }
