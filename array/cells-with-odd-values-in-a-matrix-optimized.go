90bf4d7827e6c6b92d8e4839c169cae46b4b7b60
nurzhanuly <nurzhanuly@kolesa.kz>
2021-08-24 23:29:28 +0600

    Optimized solution, peeked from discussion section, reproduced day later

--- array/cells-with-odd-values-in-a-matrix-optimized.go
+++ array/cells-with-odd-values-in-a-matrix-optimized.go
@@ -1,1 +1,22 @@
 package array
+
+func OddCellsOptimized(m int, n int, indices [][]int) int {
+	rowIndices := make(map[int]int)
+	columnIndices := make(map[int]int)
+	var result int
+
+	for _, value := range indices {
+		rowIndices[value[0]] += 1
+		columnIndices[value[1]] += 1
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if (rowIndices[i]+columnIndices[j])%2 == 1 {
+				result += 1
+			}
+		}
+	}
+
+	return result
+}
