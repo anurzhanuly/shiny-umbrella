123a84e8a89d0c81f89f35c8bba03278708adbd8
nurzhanuly <nurzhanuly@kolesa.kz>
2021-08-27 02:05:56 +0600

    complete brute force solution

--- array/findGcd.go
+++ array/findGcd.go
@@ -5,17 +5,17 @@ func FindGCD(nums []int) int {
 	var smallestNumber = nums[0]
 	var biggestNumber = nums[0]
 
-	for _, number := nums {
+	for _, number := range nums {
 		if number > biggestNumber {
 			biggestNumber = number
 		}
-		
+
 		if number < smallestNumber {
 			smallestNumber = number
 		}
 	}
 
-	for i := 1; i < biggestNumber; i++ {
+	for i := 1; i <= smallestNumber; i++ {
 		if smallestNumber%i == 0 && biggestNumber%i == 0 {
 			result = i
 		}
