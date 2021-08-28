96d85dfdd5d24eafa727f0dc7236b0695d5bcc21
nurzhanuly <nurzhanuly@kolesa.kz>
2021-08-28 21:04:17 +0600

    delete comments

--- array/diagonalSum.go
+++ array/diagonalSum.go
@@ -2,17 +2,14 @@ package array
 
 func DiagonalSum(mat [][]int) int {
 	var result int
-	//2: I can make 2 pointers, 1 starts from 0 to len(n-1) and 2nd one is opposite
 	primaryIdx := 0
 	secondaryIdx := len(mat) - 1
 	var ignoredIdx = -1
 
-	//1: find the length of the row to understand whether center is ignored
-	if len(mat)%2 == 1 {
-		//1.5: if the row is odd make block for center element
+	if secondaryIdx%2 == 1 {
 		ignoredIdx = secondaryIdx / 2
 	}
-	//3: Iterate over the rows and sum all of it
+
 	for _, row := range mat {
 		result += row[primaryIdx]
 
