cacfea583417351e91a7d1cd0ef19d9647df45ac
nurzhanuly <nurzhanuly@kolesa.kz>
2021-09-04 19:37:28 +0600

    optimized solution from discussion section

--- array/subsetXORsum.go
+++ array/subsetXORsum.go
@@ -1,5 +1,13 @@
 package array
 
 func SubsetXORSum(nums []int) int {
-	return 1
+	return find(nums, 0, 0)
+}
+
+func find(nums []int, index int, xorSum int) int {
+	if index == len(nums) {
+		return xorSum
+	}
+
+	return find(nums, index+1, xorSum^nums[index]) + find(nums, index+1, xorSum)
 }
