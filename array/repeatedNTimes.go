2e0ce09f596e0be5b938a044674bf0ab982957b8
nurzhanuly <isaxved@gmail.com>
2021-09-07 23:08:55 +0600

    stops at the first condition, since the description of the task says that nums.length == 2 * n. nums contains n + 1 unique elements. Exactly one element of nums is repeated n times.

--- array/repeatedNTimes.go
+++ array/repeatedNTimes.go
@@ -5,11 +5,8 @@ func RepeatedNTimes(nums []int) int {
 
 	for _, number := range nums {
 		elements[number] += 1
-	}
-
-	for key, value := range elements {
-		if value == len(nums)/2 {
-			return key
+		if elements[number] > 1 {
+			return number
 		}
 	}
 
