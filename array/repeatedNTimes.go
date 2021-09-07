430fb96e47ab76e55892b87ece8ea0fa7f2011ff
nurzhanuly <isaxved@gmail.com>
2021-09-07 23:03:35 +0600

    removes without additional variable

--- array/repeatedNTimes.go
+++ array/repeatedNTimes.go
@@ -2,15 +2,16 @@ package array
 
 func RepeatedNTimes(nums []int) int {
 	elements := make(map[int]int)
-	var result int
 
 	for _, number := range nums {
 		elements[number] += 1
+	}
 
-		if elements[number] == len(nums)/2 {
-			result = number
+	for key, value := range elements {
+		if value == len(nums)/2 {
+			return key
 		}
 	}
 
-	return result
+	return -1
 }
