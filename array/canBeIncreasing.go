e3f6f0e29b056d094b9cca24ef7c9c8306a44914
nurzhanuly <isaxved@gmail.com>
2021-09-08 23:12:22 +0600

    added second variable for comparison

--- array/canBeIncreasing.go
+++ array/canBeIncreasing.go
@@ -1,18 +1,21 @@
 package array
 
 func CanBeIncreasing(nums []int) bool {
-	// 1 var storing the last element
-	// 2 var store if the index for removal is found
 	var lastValue = nums[0]
+	var valueB int
 	var removeNeeded = false
-	// compare values
+
 	for i := 1; i < len(nums); i++ {
 		if nums[i] > lastValue {
+			valueB = lastValue
 			lastValue = nums[i]
 		} else {
 			if !removeNeeded {
 				removeNeeded = true
-				lastValue = nums[i]
+
+				if nums[i] > valueB {
+					lastValue = nums[i]
+				}
 
 				continue
 			}
