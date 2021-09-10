15a308c409c64f2022b586c2b4e49ebd1786e773
nurzhanuly <isaxved@gmail.com>
2021-09-10 10:34:21 +0600

    non-working solution, needs a revision

--- array/thirdMax.go
+++ array/thirdMax.go
@@ -1,5 +1,25 @@
 package array
 
-func ThirdMax(nums []int) int {
+import "math"
 
+func ThirdMax(nums []int) int {
+	max1 := math.MinInt32
+	max2 := math.MinInt32
+	max3 := math.MinInt32
+
+	for _, value := range nums {
+		if value > max1 {
+
+			if max2 < max1 {
+				if max3 < max2 {
+					max3 = max2
+				}
+				max2 = max1
+			}
+			max1 = value
+
+			continue
+		}
+
+	}
 }
