447d04fdff53efc9fef258970cf36b9ef85d6d84
nurzhanuly <isaxved@gmail.com>
2021-09-12 22:44:03 +0600

    brute force solution

--- array/finalPrices.go
+++ array/finalPrices.go
@@ -1,5 +1,18 @@
 package array
 
 func FinalPrices(prices []int) []int {
+	for i := 0; i < len(prices); i++ {
+		var tmp int
 
+		for j := i + 1; j < len(prices); j++ {
+			if prices[i] >= prices[j] {
+				tmp = prices[i] - prices[j]
+				break
+			}
+		}
+
+		prices[i] = tmp
+	}
+
+	return prices
 }
