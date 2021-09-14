119d8ca0fcc64c0caf8e9e1a2e382a1613e26b01
nurzhanuly <isaxved@gmail.com>
2021-09-14 18:54:37 +0600

    switching to python

--- data_structure/stack.go
+++ data_structure/stack.go
@@ -1,40 +1,40 @@
-package datastructure
+// package datastructure
 
-import (
-	"reflect"
-)
+// import (
+// 	"reflect"
+// )
 
-type Stack struct {
-	ItemStack [8]interface{}
-	Top       int
-}
+// type Stack struct {
+// 	ItemStack [8]interface{}
+// 	Top       int
+// }
 
-func (s *Stack) NewStack() *Stack {
-	return s
-}
+// func (s *Stack) NewStack() *Stack {
+// 	return s
+// }
 
-func (s *Stack) Push(item interface{}) {
-	if len(s.ItemStack) == s.Top {
-		s.resize(s.ItemStack[:])
-	}
-}
+// func (s *Stack) Push(item interface{}) {
+// 	if len(s.ItemStack) == s.Top {
+// 		s.resize(s.ItemStack[:])
+// 	}
+// }
 
-func (s *Stack) resize(original []interface{}) []interface{} {
+// func (s *Stack) resize(original []interface{}) []interface{} {
 
-	switch varType := reflect.TypeOf(original).String(); varType {
-	case "[]int":
-		varCopy := [len(s.ItemStack) * 2]int{}
-		varCopy = s.copyValues(original, varCopy)
-	case "[]string":
-		varCopy := [len(s.ItemStack) * 2]string{}
-	}
+// 	switch varType := reflect.TypeOf(original).String(); varType {
+// 	case "[]int":
+// 		varCopy := [len(s.ItemStack) * 2]int{}
+// 		varCopy = s.copyValues(original, varCopy)
+// 	case "[]string":
+// 		varCopy := [len(s.ItemStack) * 2]string{}
+// 	}
 
-}
+// }
 
-func (s *Stack) copyValues(original, destination []interface{}) []interface{} {
-	for index := range original {
-		destination[index] = original[index]
-	}
+// func (s *Stack) copyValues(original, destination []interface{}) []interface{} {
+// 	for index := range original {
+// 		destination[index] = original[index]
+// 	}
 
-	return destination
-}
+// 	return destination
+// }
