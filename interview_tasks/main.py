eaf18064f569077f737bd5c07227b917be8ac3a9
nurzhanuly <isaxved@gmail.com>
2022-07-29 00:10:27 +0600

    basic logic with flaws

--- interview_tasks/main.py
+++ interview_tasks/main.py
@@ -9,8 +9,23 @@ def get_test_cases(counter, test_cases):
     return get_test_cases(counter - 1, test_cases)
 
 
-def get_square_roots(values):
-    pass
+def square(values):
+    if not len(values):
+        return 0
+
+    value = int(values.pop())
+
+    return (value * value) + square(values)
+
+
+def get_square_roots(test_cases, result):
+    if not len(test_cases):
+        return result
+
+    single_case = test_cases.pop()
+    result.append(square(single_case))
+
+    return get_square_roots(test_cases, result)
 
 
 def print_values(values):
@@ -18,11 +33,12 @@ def print_values(values):
 
 
 def main():
+    container = []
     cases_count = int(input())
-    test_cases = get_test_cases(cases_count, [])
+    test_cases = get_test_cases(cases_count, container)
+    squared_values = get_square_roots(test_cases, container)
 
-    print(test_cases)
-    # squared_values = get_square_roots(test_cases)
+    print(squared_values)
     # print_values(squared_values)
 
 
